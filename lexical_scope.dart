3a99d347c6a4a746a25fc164a5a5b7f1bb7e90af
Jayanta <bjayanta.me@gmail.com>
2022-02-08 13:14:47 +0600

    update lesical scope

--- lexical_scope.dart
+++ lexical_scope.dart
@@ -8,15 +8,21 @@ void main(List<String> args) {
   void a() {
     xVar = 40; // local var
     fahimVar = 55;
+    var aVar = 100; // locar var
+
     void b() {
       xVar = 88;
       fahimVar = 89;
+      aVar = 50;
+
       void c() {
         xVar = 42;
         fahimVar = 50;
       }
     }
   }
+
+  // aVar = 23; // error
 }
 
 // xVar = 15; // error
